6a4b278162b4ab1ce853c8b18685a5a6f5f098ca
realwsq <wsq@wangshuqi.com>
2017-10-09 18:55:45 +0800

    camera recognition usable

--- Simple Image Recognition App/Simple Image Recognition App/ViewController.swift
+++ Simple Image Recognition App/Simple Image Recognition App/ViewController.swift
@@ -6,18 +6,86 @@
 //  Copyright © 2017年 wsq. All rights reserved.
 //
 import UIKit
+import AVKit
 import CoreML
+import Vision
 
-class ViewController: UIViewController, UINavigationControllerDelegate {
+class ViewController: UIViewController, UINavigationControllerDelegate, AVCaptureVideoDataOutputSampleBufferDelegate {
     
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var classifier: UILabel!
     
     var model: Inceptionv3!
     
+    let identifierLabel: UILabel = {
+        let label = UILabel()
+        label.backgroundColor = .white
+        label.textAlignment = .center
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // here is where we start up the camera
+        // for more details visit: https://www.letsbuildthatapp.com/course_video?id=1252
+        let captureSession = AVCaptureSession()
+        captureSession.sessionPreset = .photo
+        
+        guard let captureDevice = AVCaptureDevice.default(for: .video) else { return }
+        guard let input = try? AVCaptureDeviceInput(device: captureDevice) else { return }
+        captureSession.addInput(input)
+        
+        captureSession.startRunning()
+        
+        let previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        view.layer.addSublayer(previewLayer)
+        previewLayer.frame = view.frame
+        
+        let dataOutput = AVCaptureVideoDataOutput()
+        dataOutput.setSampleBufferDelegate(self, queue: DispatchQueue(label: "videoQueue"))
+        captureSession.addOutput(dataOutput)
+        
+        setupIdentifierConfidenceLabel()
+    }
+    
+    fileprivate func setupIdentifierConfidenceLabel() {
+        view.addSubview(identifierLabel)
+        identifierLabel.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -32).isActive = true
+        identifierLabel.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        identifierLabel.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+        identifierLabel.heightAnchor.constraint(equalToConstant: 50).isActive = true
+    }
+    
+    func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
+        //        print("Camera was able to capture a frame:", Date())
+        
+        guard let pixelBuffer: CVPixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) else { return }
+        
+        // !!!Important
+        // make sure to go download the models at https://developer.apple.com/machine-learning/ scroll to the bottom
+        guard let model = try? VNCoreMLModel(for: Inceptionv3().model) else { return }
+        let request = VNCoreMLRequest(model: model) { (finishedReq, err) in
+            
+            //perhaps check the err
+            
+            //            print(finishedReq.results)
+            
+            guard let results = finishedReq.results as? [VNClassificationObservation] else { return }
+            
+            guard let firstObservation = results.first else { return }
+            
+            print(firstObservation.identifier, firstObservation.confidence)
+            
+            DispatchQueue.main.async {
+                self.identifierLabel.text = "\(firstObservation.identifier) \(firstObservation.confidence * 100)"
+            }
+            
+        }
+        
+        try? VNImageRequestHandler(cvPixelBuffer: pixelBuffer, options: [:]).perform([request])
     }
     
     override func viewWillAppear(_ animated: Bool) {
