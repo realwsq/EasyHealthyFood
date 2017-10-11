3d1196162f0937616d6508c3ba81d1caf2fa61d1
realwsq <wsq@wangshuqi.com>
2017-10-11 14:59:45 +0800

    can be tabbed in tabbedApp

---
+++ tabbedApp/tabbedApp/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  tabbedApp
+//
+//  Created by 王书琪 on 2017/10/11.
+//  Copyright © 2017年 wsq. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
